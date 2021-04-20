8e3379266950b02b8dfb9652a99f96ba4b722d56
Libin <llu@flatironinstitute.org>
2021-04-19 21:01:35 -0400

    test

--- .github/workflows/C++_build_win.ps1
+++ .github/workflows/C++_build_win.ps1
@@ -6,5 +6,5 @@ Copy-Item -Path make.inc.windows_msys -Destination make.inc
 
 # call make
 Set-Variable repo_root -Value ([IO.Path]::Combine($PSScriptRoot, '..', '..'))
-c:\msys64\usr\bin\env MSYSTEM=MINGW64 c:\msys64\usr\bin\bash.exe -lc "cd '$repo_root' && make print-EXAMPLES"
+c:\msys64\usr\bin\env MSYSTEM=MINGW64 c:\msys64\usr\bin\bash.exe -lc "cd '$repo_root' && make print-EXAMPLES && make spreadtestall"
 if (-not $?) {throw "Failed make"}
