ed35ae2c6d5cc29886d204e2b4995101baca790c
lu1and10 <llu@flatironinstitute.org>
2022-02-19 16:12:33 -0500

    test

--- .github/workflows/python_test_win.ps1
+++ .github/workflows/python_test_win.ps1
@@ -1,4 +1,4 @@
-python -m pip install --no-index --find-links=./wheelhouse/ finufft
+python -m pip install finufft
 if (-not $?) {throw "Failed to pip install finufft"}
 python python/test/run_accuracy_tests.py
 if (-not $?) {throw "Tests failed"}
